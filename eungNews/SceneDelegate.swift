8022ff0e22de82f2008b5e9310307cf593b1ee75
eung7 <wannabeeung@gmail.com>
2022-03-12 17:46:47 +0900

    adding .systemOrange

--- eungNews/SceneDelegate.swift
+++ eungNews/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = UINavigationController(rootViewController: NewsListViewController())
         window?.backgroundColor = .systemBackground
+        window?.tintColor = .systemOrange
         window?.makeKeyAndVisible()
     }
 }
