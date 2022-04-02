596a171d22c271532a0d7592531cceec0a1d79e1
eung7 <wannabeeung@gmail.com>
2022-04-02 20:29:40 +0900

    First

---
+++ eungNews/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  eungNews
+//
+//  Created by 김응철 on 2022/03/08.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ eungNews/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  eungNews
+//
+//  Created by 김응철 on 2022/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
